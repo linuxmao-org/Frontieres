d3e250e45b841629d484deebdbff6d3c4701de5f
Olivier Humbert <trebmuh@users.noreply.github.com>
2018-10-02 21:12:56 +0200

    Update SoundRect.cpp

--- SoundRect.cpp
+++ SoundRect.cpp
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// FRONTIERES:  An interactive granular sampler.
+// FRONTIÈRES:  An interactive granular sampler.
 //------------------------------------------------------------------------------
 // More information is available at
 //     http::/ccrma.stanford.edu/~carlsonc/256a/Borderlands/index.html
@@ -22,7 +22,7 @@
 
 //
 //  SoundRect.cpp
-//  Frontieres
+//  Frontières
 //
 //  Created by Christopher Carlson on 11/30/11.
 //
